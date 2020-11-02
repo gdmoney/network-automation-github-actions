41a3874e58626dc26f9d08fdb8952e7bbb0ed9dd
George Davitiani <george.davitiani@gmail.com>
2020-11-02 15:48:15 -0800

    added project files

---
+++ my_devices_2.py
@@ -0,0 +1,45 @@
+username = 'siteadmin'
+password = 'H0ck3y*'
+
+ROUTER_1 = {
+    'device_type': 'cisco_ios',
+    'ip': '172.29.67.110',
+    'username': username,
+    'password': password,
+}
+
+ROUTER_2 = {
+    'device_type': 'cisco_ios',
+    'ip': '172.29.67.111',
+    'username': username,
+    'password': password,
+}
+
+ROUTER_3 = {
+    'device_type': 'cisco_ios',
+    'ip': '172.29.67.112',
+    'username': username,
+    'password': password,
+}
+
+ROUTER_4 = {
+    'device_type': 'cisco_ios',
+    'ip': '172.29.67.113',
+    'username': username,
+    'password': password,
+}
+
+ROUTER_5 = {
+    'device_type': 'cisco_ios',
+    'ip': '172.29.67.114',
+    'username': username,
+    'password': password,
+}
+
+device_list = [
+        ROUTER_1,
+        ROUTER_2,
+        ROUTER_3,
+        ROUTER_4,
+        ROUTER_5,
+    ]
