51bc9bd5c56d84109e4b93089f5538cf95ab36a2
george.davitiani@hey.com <46586741+gdmoney@users.noreply.github.com>
2023-10-18 12:36:40 -0700

    Update _access.py

--- _access.py
+++ _access.py
@@ -4,11 +4,11 @@ from _all_devices import access_switches as devices
 
 def task(a_device):
     # with open('config_replace_access_switch') as f:
-    config_replace = 'config replace tftp://172.17.0.2/config_file_access_switch force'
+        # config_replace = f.read().splitlines()
 
     session = ConnectHandler(**a_device)
-    output = session.send_config_set(config_replace)
-    # output = session.send_command('config replace tftp://172.17.0.2/config_file_access_switch force')
+    # output = session.send_config_set(config_replace)
+    output = session.send_command('config replace tftp://172.17.0.2/config_file_access_switch force')
     output += session.save_config()
     # print (output)
 
