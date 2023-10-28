b57cf0b92cb9c8dadd0f62813a944c53e5b46f7a
george.davitiani@hey.com <46586741+gdmoney@users.noreply.github.com>
2023-10-28 15:00:51 -0700

    version 2 code update

---
+++ router_1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+curl -X 'POST' \
+  'http://172.18.50.1:8085/api/v3/jobs/push' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCJ9.SENSdIUM5ZI7BwHH7mVw2cHyZwMzSQCngz0CNBcyAuU' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "commands": [
+    "config replace tftp://172.17.0.2/config_file_router_1 force"
+  ],
+  "requireEnableMode": true,
+  "requireConfigureMode": false,
+  "tagUuids": [
+    "6a31a8fd-eea7-430a-a126-25d00a3e5928"
+  ],
+  "advancedSettings": {
+    "promptMatchingMode": "LEARNING",
+    "overrideTimeouts": false,
+    "timeout": 0,
+    "overrideCredentials": false,
+    "username": "string",
+    "password": "string",
+    "enablePassword": "string",
+    "configurePassword": "string"
+  }
+}'
---
+++ router_2.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+curl -X 'POST' \
+  'http://172.18.50.1:8085/api/v3/jobs/push' \
+  -H 'accept: application/json' \
+  -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCJ9.SENSdIUM5ZI7BwHH7mVw2cHyZwMzSQCngz0CNBcyAuU' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "commands": [
+    "config replace tftp://172.17.0.2/config_file_router_2 force"
+  ],
+  "requireEnableMode": true,
+  "requireConfigureMode": false,
+  "tagUuids": [
+    "27ac3847-ddcf-4109-9984-544fbb52a80f"
+  ],
+  "advancedSettings": {
+    "promptMatchingMode": "LEARNING",
+    "overrideTimeouts": false,
+    "timeout": 0,
+    "overrideCredentials": false,
+    "username": "string",
+    "password": "string",
+    "enablePassword": "string",
+    "configurePassword": "string"
+  }
+}'
