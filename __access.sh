b2f367f8ad1c8bf502e119c579a5886d54e836c3
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2025-01-07 12:08:25 -0800

    Create __access.sh

---
+++ __access.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+API_URL="http://192.168.255.5:8085/api/v3/jobs/push"
+AUTH_TOKEN="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhdXRoMCJ9.SENSdIUM5ZI7BwHH7mVw2cHyZwMzSQCngz0CNBcyAuU"
+TAG_UUID="b29da665-3147-4790-a775-ac8ed583231b"
+
+curl -X "POST" "${API_URL}" \
+  -H "accept: application/json" \
+  -H "Authorization: "${AUTH_TOKEN}"" \
+  -H "Content-Type: application/json" \
+  -d "{
+  "commands": [
+    "config replace tftp://172.18.0.2/config_file_access_switch force",
+    "write memory"
+  ],
+  "requireEnableMode": true,
+  "requireConfigureMode": false,
+  "tagUuids": [
+    "${TAG_UUID}"
+  ],
+  "advancedSettings": {
+    "promptMatchingModeEnum": "LEARNING",
+    "overrideTimeouts": false,
+    "timeout": 0,
+    "overrideCredentials": false,
+    "username": "string",
+    "password": "string",
+    "enablePassword": "string",
+    "configurePassword": "string"
+  }
+}'
