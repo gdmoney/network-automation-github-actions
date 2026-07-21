be44885a6257023c3e1a179b71bffdd026f5a960
George Davitiani <46586741+gdmoney@users.noreply.github.com>
2026-07-21 14:42:45 -0700

    Update TAG_UUID variable in _router_2.sh

--- _router_2.sh
+++ _router_2.sh
@@ -3,7 +3,7 @@
 # Variables
 API_URL="http://192.168.255.5:8085/api/v3/jobs/push"
 AUTH_TOKEN="${AUTH_TOKEN}"
-TAG_UUID="27ac3847-ddcf-4109-9984-544fbb52a80f"
+TAG_UUID="db01475b-701c-4cee-8e9b-b250b38131e0"
 
 # Lookup server's IP address
 TFTP_SERVER_HOSTNAME="ubuntu"
